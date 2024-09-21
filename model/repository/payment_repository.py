d10aecbd845cb858fdbb83f5814dc282b2eb910f
amirs <amirshayamemami@gmail.com>
2024-09-21 12:58:00 +0330

    added payment_database and payment_repository

--- model/repository/payment_repository.py
+++ model/repository/payment_repository.py
@@ -2,14 +2,16 @@ import mysql.connector
 
 from model.entity.payment import Payment
 
+
 class PaymentRepository:
-    self.connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="root123",
-        database="office_db"
-    )
-    self.cursor = self.connection.cursor()
+    def connect(self):
+        self.connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="root123",
+            database="office_db"
+        )
+        self.cursor = self.connection.cursor()
 
 def disconnect(self):
     self.cursor.close()
