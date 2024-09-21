5a2d90ec79553c540070ad127ec8c5d047ccda56
amirs <amirshayamemami@gmail.com>
2024-09-21 13:06:12 +0330

    added payment_database and payment_repository

--- model/repository/payment_repository.py
+++ model/repository/payment_repository.py
@@ -17,3 +17,24 @@ class PaymentRepository:
         self.cursor.close()
         self.connection.close()
 
+    def save(self, payment):
+        self.connect()
+        self.cursor.execute("insert into payment_tbl (amount, date, person) values (%s,%s,%s)",
+                            [payment.amount, payment.date_time, payment.person])
+        self.connection.commit()
+        self.disconnect()
+
+    def remove(self, id):
+        self.connect()
+        self.cursor.execute("delete from payment_tbl where id=%s", [id])
+        self.connection.commit()
+        self.disconnect()
+
+    def find_by_id(self, id):
+        self.connect()
+        self.cursor.execute("select * from payment_tbl where id=%s", [id])
+        emp = self.cursor.fetchone()
+        self.disconnect()
+        if emp:
+            emp = Payment(*emp)
+            return emp
