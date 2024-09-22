24d91ce45fb28a5020c5b4bd8fcd5d97fedf7b1b
NimaTheCreator <sanatinima5@gmail.com>
2024-09-22 17:23:39 +0330

    product service added

--- model/entity/product_validator.py
+++
@@ -1,6 +0,0 @@
-if (re.match(r"^[1-9]{7}$", id)
-        and re.match(r"^[a-zA-z\s]{2,20}$", name)
-        and re.match(r"^[a-zA-z\s]{2,20}$", brand)
-        and re.match(r"^[a-zA-z\s]{2,20}$", model)
-        and re.match(r"^[1-9a-zA-Z]{20}$", barcode)
-        and type(buy_price) == int and type(sell_price) == int):
