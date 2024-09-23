ed9f1721c8c90558983583e25f2ead3858e290f5
NimaTheCreator <sanatinima5@gmail.com>
2024-09-24 00:07:38 +0330

    (commited bt NimaTheCreator)

--- model/entity/product_validation.py
+++ model/entity/product_validation.py
@@ -5,13 +5,13 @@ def id_validator(id):
     if type(id) == int and re.match(r"^[1-9]{7}$", id):
         return id
 def name_validator(name):
-    if type(name) == str and re.match(r"^[a-zA-Z]{2,20}$", name):
+    if type(name) == str and re.match(r"^[a-zA-Z\s]{2,20}$", name):
         return name
 def brand_validator(brand):
-    if type(brand) == str and re.match(r"^[a-zA-Z1-9]{2,20}$", brand):
+    if type(brand) == str and re.match(r"^[a-zA-Z1-9\s]{2,20}$", brand):
         return brand
 def model_validator(model):
-    if type(model) == str and re.match(r"^[a-zA-Z1-9]{2,20}$", model):
+    if type(model) == str and re.match(r"^[a-zA-Z1-9\s]{2,20}$", model):
         return model
 def barcode_validator(barcode):
     if type(barcode) == str and re.match(r"^[a-zA-Z1-9]{10}$", barcode):
