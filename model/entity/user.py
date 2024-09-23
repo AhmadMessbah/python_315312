b5762987af74752f44779079f8a2671110b1d997
panizabt <abootalebipaniz@gmail.com>
2024-09-23 21:33:09 +0330

    Update user.py

--- model/entity/user.py
+++ model/entity/user.py
@@ -1,7 +1,10 @@
+import re
+
 from user_validation import *
 
+
 class User:
-    def __init__(self, id,name,family,birth_date,username,password,is_active):
+    def __init__(self, id, name, family,birth_date,username,password,is_active):
         self.id = id
         self.name = name
         self.family = family
@@ -24,18 +27,43 @@ class User:
 
     def get_name(self):
         return self._name
-            
+
     def set_name(self, name):
         self._name = name_validator(name)
 
     def get_family(self):
-        return self._family
-    
+        return self._pages
+
     def set_family(self, family):
         self._family = family_validator(family)
+
+    def get_birth_date(self):
+        return self._birth_date
+
+    def set_birth_date(self, birth_date):
+        self._birth_date = positive_int(birth_date)
+
+    def get_username(self):
+        return self._username
+
+    def set_username(self, username):
+        self._username = positive_int(username)
+
+    def get_is_active(self):
+         return self._is_active
+
+    def set_is_active(self, is_active):
+        self._is_active = boolian(is_active)
 
 
 
     id = property(get_id, set_id)
-    name= property(get_name, set_name)
+    name = property(get_name, set_name)
     family = property(get_family, set_family)
+    birth_date = property(get_birth_date, set_birth_date)
+    username = property(get_username, set_username)
+    password = property(get_password, set_password)
+    is_active = property(get_is_active, set_is_active)
+
+
+
