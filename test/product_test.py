5f57d626da9d384c54a6e450d1506090de5f60fc
NimaTheCreator <sanatinima5@gmail.com>
2024-09-24 00:08:28 +0330

    (commited by NimaTheCreator)

--- test/product_test.py
+++ test/product_test.py
@@ -1,6 +1,6 @@
 from model.entity.product import Product
 
 
-product_test = Product( 1234567, "DB12", "Aston Martin", "DB series","2dk4576vh","24000","248000")
+product_test = Product( 1234567, "DB12", "Aston Martin", "DB series","2dk4576av","24000","248000")
 print(product_test)
 print(product_test.to_tuple())
