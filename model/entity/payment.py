0c186a924e61cb9e5a5480a4d5b21180f2a0120d
bardiazaghari <bardia.zaghari@yahoo.com>
2024-09-29 17:11:59 +0330

    chanded by Bardia

--- model/entity/payment.py
+++ model/entity/payment.py
@@ -8,7 +8,6 @@ class Payment:
         self.account_id = account_id
         self.amount = amount
         self.date_time = datetime.now()
-        self.person = person
 
     def __repr__(self):
         return f"{self.__dict__}"
@@ -16,27 +15,39 @@ class Payment:
     def to_tuple(self):
         return tuple(self.__dict__.values())
 
-    def get_account_id(self):
-        return self.account_id
+    @property
+    def id(self):
+        return self._id
 
-    def set_account_id(self, account_id):
-        self.account_id = account_int(account_id)
+    @id.setter
+    def id(self, id):
+        self._id = id
 
-    def get_amount(self):
-        return self.amount
+    @property
+    def account_id(self):
+        return self._account_id
 
-    def set_amount(self, amount):
-        self.amount = amount_int(amount)
+    @account_id.setter
+    def account_id(self, account_id):
+        self._account_id = account_int(account_id)
 
-    def get_person(self):
-        return self.person
+    @property
+    def amount(self):
+        return self._amount
 
-    def set_person(self, person):
-        self.person = person_validator(person)
+    @amount.setter
+    def amount(self, amount):
+        self._amount = amount_int(amount)
 
-    account_id = property(get_account_id, set_account_id)
-    amount = property(get_amount, set_amount)
-    person = property(get_person, set_person)
+    @property
+    def person(self):
+        return self._person
+
+    @person.setter
+    def person(self, person):
+        self._person = person_validator(person)
+
+
 
 # if isinstance(date_time, datetime) else datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S")
 
