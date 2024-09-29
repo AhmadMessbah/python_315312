96493107b525071550c9a7a9fc23dc63e0a98b07
nima-kazemi627 <nkazemi627@gmail.com>
2024-09-29 17:14:48 +0330

    controller updated...

--- controller/lesson_controller.py
+++ controller/lesson_controller.py
@@ -7,20 +7,19 @@ from model.service.lesson_service import LessonService
 class LessonController:
 
     @classmethod
-    def save(cls, title, week_day, start_date, start_time, end_time):
+    def save(cls, id, title, week_day, start_date, start_time, end_time):
         try:
 
-            lesson = Lesson(None,title, week_day, start_date,start_time,end_time)
+            lesson = Lesson(id, title, week_day, start_date, start_time, end_time)
             LessonService.save(lesson)
-            return True , "lesson saved"
+            return True, "lesson saved"
         except Exception as e:
             return False, str(e)
 
-
     @classmethod
-    def edit(cls,id, title, week_day, start_date,start_time,end_time):
+    def edit(cls, id, title, week_day, start_date, start_time, end_time):
         try:
-            lessn = Lesson(id,title, week_day, start_date,start_time,end_time)
+            lessn = Lesson(id, title, week_day, start_date, start_time, end_time)
             LessonService.edit(lessn)
             return True, "lesson edited"
         except Exception as e:
@@ -38,5 +37,12 @@ class LessonController:
     def find_all(cls):
         try:
             return True, LessonService.find_all(cls)
+        except Exception as e:
+            return False, str(e)
+
+    @classmethod
+    def find_by_id(cls, id):
+        try:
+            return True, LessonService.find_by_id(id)
         except Exception as e:
             return False, str(e)
