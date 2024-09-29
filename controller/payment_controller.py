dcda582d25c371b164738d4427c67ae1c69e4930
AidaShams <aidashmas77@gmail.com>
2024-09-29 21:30:39 +0330

    payment controller update using classmethod

--- controller/payment_controller.py
+++ controller/payment_controller.py
@@ -3,39 +3,44 @@ from model.service.payment_service import PaymentService
 
 
 class PaymentController:
-    def __init__(self):
-        self.service = PaymentService()
 
-    def process_payment(self, id, account_id, amount, person):
+    @classmethod
+    def save(cls, id, account_id, amount, person):
         # payment entity setter
-        pay = Payment(None, account_id, amount, person)
-        error = self.service.process_payment(pay)
-        if not error:
+        try:
+            pay = Payment(None, account_id, amount, person)
+            PaymentService.save(pay, amount, person)
             return True, "Info: Payment Saved!"
-        else:
-            return False, error
+        except Exception as e:
+            return False, str(e)
 
-    def edit_payment(self, id, account_id, amount, person):
-        pay = Payment(id, account_id, amount, person)
-        error = self.service.edit_payment(pay)
-        if not error:
+    @classmethod
+    def edit(cls, id, account_id, amount, person):
+        try:
+            pay = Payment(id, account_id, amount, person)
+            PaymentService.edit(pay, account_id, amount, person)
             return True, "Payment Has Been Edited!"
-        else:
-            return False, error
+        except Exception as e:
+            return False, str(e)
 
-    def remove_payment(self, id):
-        error = self.service.remove_payment(id)
-        if not error:
-            return True, "Payment Has Been Removed!"
-        else:
-            return False, error
+    @classmethod
+    def remove(cls, id):
+        try:
+            PaymentService.remove(id)
+            return True, "Info: Payment Removed!"
+        except Exception as e:
+            return False, str(e)
 
-    def find_payment_by_id(self, id):
-        payment = self.service.find_payment_by_id(id)
-        if payment:
-            return "Payment Found!", payment
-        else:
-            return f"Payment With ID {id} Not Found!"
+    @classmethod
+    def find_all(cls):
+        try:
+            return True, PaymentService.find_all()
+        except Exception as e:
+            return False, str(e)
 
-    def get_all_payments(self):
-        return self.service.get_all_payments()
+    @classmethod
+    def find_by_id(cls, id):
+        try:
+            return True, PaymentService.find_by_id(id)
+        except Exception as e:
+            return False, str(e)
--- model/service/payment_service.py
+++ model/service/payment_service.py
@@ -1,18 +1,18 @@
 from model.repository.payment_repository import PaymentRepository as repo
 from model.entity.payment import Payment
 
-class PaymentService:
 
+class PaymentService:
 
     @classmethod
     def save(cls, account, amount, person):
         new_payment = Payment(id, account, amount, person)
         return cls.repo.save(new_payment)
-    
+
     @classmethod
     def edit(cls, id, amount, date_time, person):
         try:
-            payment = repo.find_by_all(id)
+            payment = repo.find_by_id(id)
             repo.edit(payment)
             return f"Payment with ID {id} updated."
         except:
