7204904db5a9f0b7216943ff909975d901d4826b
Shayan Emami <amirshayanemami@gmail.com>
2024-09-29 17:38:29 +0330

    updated payment_controller.py

--- controller/payment_controller.py
+++ controller/payment_controller.py
@@ -4,40 +4,40 @@ from model.service.payment_service import PaymentService
 
 class PaymentController:
     @classmethod
-    def process_payment(cls, account_id, amount, person):
+    def save(cls, account_id, amount, person):
         try:
             pay = Payment(None, account_id, amount, person)
-            PaymentService.process_payment(pay)
+            PaymentService.save(pay)
             return True, 'Payment Saved'
         except Exception as e:
             return False, str(e)
 
     @classmethod
-    def edit_payment(cls, id, account_id, amount, person):
+    def edit(cls, id, account_id, amount, person):
         try:
             pay = Payment(id, account_id, amount, person)
-            PaymentService.edit_payment(pay)
+            PaymentService.edit(pay)
             return True, 'Payment Edited'
         except Exception as e:
             return False, str(e)
 
     @classmethod
-    def remove_payment(cls, id):
+    def remove(cls, id):
         try:
-            PaymentService.remove_payment(id)
+            PaymentService.remove(id)
             return True, 'Payment Removed'
         except Exception as e:
             return False, str(e)
 
     @classmethod
-    def get_all_payments(cls):
+    def find_all(cls):
         try:
-            return True, PaymentService.get_all_payments()
+            return True, PaymentService.find_all()
         except Exception as e:
             return False, str(e)
 
     @classmethod
-    def find_payment_by_id(cls, id):
+    def find_by_id(cls, id):
         try:
             return True, PaymentService.find_by_id(id)
         except Exception as e:
