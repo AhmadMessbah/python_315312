ed2ba458f30ee48b0c58a73c0ce088fe6200ce68
Nika <nika.mohseni85@gmail.com>
2024-10-01 17:17:31 +0330

    product_view updated

--- view/product_view.py
+++ view/product_view.py
@@ -6,13 +6,6 @@ from controller.product_controller import ProductController
 from view.component import LabelWithEntry, Table
 
 class ProductView:
-    def clear_table(self):
-        for item in self.table.get_children():
-            self.table.delete(item)
-
-    def show_on_table(self):
-        for product in self.controller.find_all():
-            self.table.insert("", END, values=product.to_tuple())
 
     def reset_form(self):
         self.id.set(0)
@@ -22,8 +15,6 @@ class ProductView:
         self.barcode.set(0)
         self.buy_price.set(0)
         self.sell_price.set(0)
-        self.clear_table()
-        self.show_on_table()
 
     def table_click(self, event):
         item_id = self.table.focus()
@@ -38,15 +29,7 @@ class ProductView:
         self.sell_price.set(pro[6])
 
     def save_click(self):
-        status, message = self.controller.save(
-            self.id.get(),
-            self.name.get(),
-            self.brand.get(),
-            self.model.get(),
-            self.barcode.get(),
-            self.buy_price.get(),
-            self.sell_price.get()
-        )
+        status, message =
         if status:
             msg.showinfo("Save", message)
             self.reset_form()
@@ -79,60 +62,18 @@ class ProductView:
                 msg.showerror("Remove Error", message)
 
     def __init__(self):
-        self.controller = ProductController()
 
         win = Tk()
         win.geometry("600x300")
 
-        Label(win, text="Id").place(x=20, y=20)
-        Label(win, text="Name").place(x=20, y=60)
-        Label(win, text="brand").place(x=20, y=100)
-        Label(win, text="model").place(x=20, y=140)
-        Label(win, text="barcode").place(x=20, y=180)
-        Label(win, text="buy_price").place(x=20, y=220)
-        Label(win, text="sell_price").place(x=20, y=260)
+        self.id = LabelWithEntry(win, "Id", 20, 20, data_type="int", state="readonly")
+        self.name = LabelWithEntry(win, "Name", 20, 60)
+        self.brand = LabelWithEntry(win, "Brand", 20, 100)
+        self.model = LabelWithEntry(win, "Model", 20, 140, data_type="int")
+        self.barcode = LabelWithEntry(win, "barcode", 20, 180, data_type="int")
+        self.buy_price = LabelWithEntry(win, "buy_price", 20, 220, data_type="int")
+        self.sell_price = LabelWithEntry(win, "sell_price", 20, 260, data_type="int")
 
-
-        self.id = IntVar()
-        self.name = StringVar()
-        self.brand = StringVar()
-        self.model = StringVar()
-        self.barcode = IntVar()
-        self.buy_price = IntVar()
-        self.sell_price = IntVar()
-
-        Entry(win, textvariable=self.id, state="readonly").place(x=80, y=20)
-        Entry(win, textvariable=self.name).place(x=80, y=60)
-        Entry(win, textvariable=self.brand).place(x=80, y=100)
-        Entry(win, textvariable=self.model).place(x=80, y=140)
-        Entry(win, textvariable=self.barcode).place(x=80, y=140)
-        Entry(win, textvariable=self.buy_price).place(x=80, y=140)
-        Entry(win, textvariable=self.sell_price).place(x=80, y=140)
-
-
-        self.table = ttk.Treeview(win, columns=(1, 2, 3, 4), show="headings")
-        self.table.place(x=250, y=20)
-
-        self.table.heading(1, text="Id")
-        self.table.heading(2, text="Name")
-        self.table.heading(3, text="Brand")
-        self.table.heading(4, text="Model")
-        self.table.heading(1, text="Barcode")
-        self.table.heading(2, text="buy_price")
-        self.table.heading(3, text="sell_price")
-
-
-        self.table.column(1, width=60)
-        self.table.column(2, width=100)
-        self.table.column(3, width=100)
-        self.table.column(4, width=60)
-        self.table.column(1, width=60)
-        self.table.column(2, width=100)
-        self.table.column(3, width=100)
-
-
-        self.table.bind("<ButtonRelease-1>", self.table_click)
-        self.table.bind("<KeyRelease>", self.table_click)
 
         Button(win, text="Save", width=10, command=self.save_click).place(x=100, y=180)
         Button(win, text="Edit", width=10, command=self.edit_click).place(x=100, y=210)
