90e39f50af819d8fcca424cf2871c317e23bf6ac
panizabt <abootalebipaniz@gmail.com>
2024-10-04 13:03:31 +0330

    Update user_test.py

--- test/user_test.py
+++ test/user_test.py
@@ -3,6 +3,15 @@ from model.service.employee_service import UserService
 from datetime import date
 
 b_d = date(2000,11,10)
-user = User( 1, "alireza", "alipour", b_d, "ali", "ali12345", False)
+user1 = User(1, "alireza", "alipour", b_d, "ali", "ali12345", False)
+print(user1)
+print(user1.to_tuple())
 
-UserService.edit(user)
+user_service = UserService()
+
+user_service.save(user1)
+UserService.edit(user1)
+UserService.delete()
+
+
+print(User.Controller.save(2,"reza","rezapour",b_d ,"reza","reza12345", True))
