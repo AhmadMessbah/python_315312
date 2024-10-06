5635778296117579e4fa37148878b8c10e67850a
NimaTheCreator <sanatinima5@gmail.com>
2024-10-06 13:31:55 +0330

    (commited by NimaTheCreator)

--- model/entity/product.py
+++ model/entity/product.py
@@ -1,61 +1,79 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from email.policy import default
 
-from model.entity.product_validation import *
 from model.entity.base import Base
+from model.tools.validation import Validation
+from sqlalchemy import Column, Integer, String
+
 
 class Product(Base):
     __tablename__ = "product_tbl"
 
     _id = Column("id", Integer, primary_key=True, autoincrement=True)
-    _name = Column("name",String(20), nullable=False)
+    _name = Column("name", String(20), nullable=False)
     _brand = Column("brand", String(20), nullable=False)
-    _barcode = Column("pages", String(20), nullable=False)
+    _model = Column("model", String(20), nulable = False)
+    _barcode = Column("barcode", String(20), nullable=False)
     _buy_price = Column("buy_price", Integer, default = 0)
-    _sell_price = Column("sell_price" ,Integer, default = 0)
+    _sell_price = Column("sell_price", Integer, default = 0)
 
-
-    def __init__(self, id, name, brand, model,barcode,buy_price,sell_price):
+    def __init__(self, id, name, family, age):
         self.id = id
         self.name = name
-        self.brand = brand
-        self.model = model
-        self.barcode = barcode
-        self.buy_price = buy_price
-        self.sell_price = sell_price
+        self.family = family
+        self.age = age
 
-    def get_name(self):
-        return self.name
-    def set_name(self, name):
-        self.name = no_numbers(name)
+    @property
+    def id(self):
+        return self._id
 
-    def get_brand(self):
-        return self.brand
-    def set_brand(self, brand):
-        self.brand = no_numbers(brand)
+    @id.setter
+    def id(self, id):
+        self._id = id
 
-    def get_model(self):
-        return self.model
-    def set_model(self, model):
-        self.model = string_validator(model)
+    @property
+    def name(self):
+        return self._name
 
-    def get_barcode(self):
-        return self.barcode
-    def set_barcode(self, barcode):
-        self.barcode = string_validator(barcode)
+    @name.setter
+    def name(self, name):
+        self._name = Validation.name_validator(name, "Invalid Name")
 
-    def get_buy_price(self):
-        return self.buy_price
-    def set_buy_price(self, buy_price):
-        self.buy_price = positive_int(buy_price)
+    @property
+    def brand(self):
+        return self._brand
 
-    def get_sell_price(self):
-        return self.sell_price
-    def set_sell_price(self, sell_price):
-        self.sell_price = positive_int(sell_price)
+    @brand.setter
+    def family(self, brand):
+        self._brand = Validation.family_validator(brand, "Invalid Family")
 
-    name = property(get_name, set_name)
-    brand = property(get_brand, set_brand)
-    model = property(get_model, set_model)
-    barcode = property(get_barcode, set_barcode)
-    buy_price = property(get_buy_price, set_buy_price)
-    sell_price = property(get_sell_price, set_sell_price)
+    @property
+    def model(self):
+        return self._model
+
+    @model.setter
+    def age(self,model):
+        self._model = model
+
+    @property
+    def barcode(self):
+        return self._barcode
+
+    @barcode.setter
+    def barcode(self, barcode):
+        self._barcode = barcode
+
+    @property
+    def buy_price(self):
+        return self._buy_price
+
+    @buy_price.setter
+    def buy_price(self, buy_price):
+        self._buy_price = buy_price
+
+    @property
+    def sell_price(self):
+        return self._sell_price
+
+    @sell_price.setter
+    def sell_price(self, sell_price):
+        self._sell_price = sell_price
--- test/product_test.py
+++ test/product_test.py
@@ -1,8 +1,8 @@
-from controller.product_controller import ProductController
-controller = ProductController()
+#from controller.product_controller import ProductController
+#controller = ProductController()
 from model.entity.product import Product
-print(controller.save("ban","boom","dsh",1234567891,2000,300))
+#print(controller.save("ban","boom","dsh",1234567891,2000,300))
 prod = Product("1234567","DB12", "Aston Martin", "Db series",'46r6grt6','240000',"248000")
-
+print(prod)
 
 
