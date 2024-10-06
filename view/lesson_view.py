918d42e13bb8fb1be108e5582c621ee03bb029cc
Z-naderi1 <rasta.naderi.2022@gmail.com>
2024-10-06 22:50:54 +0330

    chenge some error a bout view and test from project

--- view/lesson_view.py
+++ view/lesson_view.py
@@ -23,32 +23,32 @@ class LessonView:
     @staticmethod
 
     def table_click(self, selected_item):
-                print(selected_item)
+     print(selected_item)
 
 
-    def save_click(self):
+     def save_click(self):
         status, message = LessonController.save(self.title.get(), self.week_day.get(), self.start_date.get())
         if status:
-            msg.showinfo("Save", f"new lesson save")
+            msg.showinfo("Save",message)
             self.reset_form()
         else:
-            msg.showerror("Save Error", f"we can't save the lesson")
+            msg.showerror("Save Error",message)
 
 
 
     def edit_click(self):
-        status, message = self.controller.edit(self.title.get(), self.week_day.get(), self.start_date.get(), self.age.get())
+        status, message =LessonController.edit(self.id.get(),self.title.get(), self.week_day.get(), self.start_date.get(), self.age.get())
         if status:
             msg.showinfo("Edit", message)
             self.reset_form()
         else:
-            msg.showerror("edit Error", f"we can't edit the lesson")
+            msg.showerror("edit Error",message)
 
 
 
     def remove_click(self):
-        if msg.askyesno("Remove Employee", "Are you sure?"):
-            status, message = self.controller.remove(self.id.get())
+        if msg.askyesno("Remove Employee","are you sure?"):
+            status, message =LessonController.remove(self.id.get())
             if status:
                 msg.showinfo("Remove", message)
                 self.reset_form()
@@ -61,56 +61,26 @@ class LessonView:
         self.window.geometry('500x500')
         self.window.title("Lesson")
 
-        tkinter.Label(self.window, text="Id").place(x=20, y=10)
-        tkinter.Entry(self.window).place(x=80, y=10)
 
-        tkinter.Label(self.window, text="Title").place(x=20, y=50)
-        tkinter.Entry(self.window).place(x=80, y=50)
+        self.id = LabelWithEntry(self.window, "Id", 20, 20, data_type="int", state="readonly")
+        self.title = LabelWithEntry(self.window, "title", 20, 60)
+        self.week_day = LabelWithEntry(self.window, "week day", 20, 100)
+        self.start_time= LabelWithEntry(self.window, "start time", 20, 140, data_type="int")
+        self.start_date= LabelWithEntry(self.window,"Start Date", 20, 180, data_type="int")
+        self.end_time= LabelWithEntry(self.window, "End Date", 20, 200, data_type="int")
 
-        tkinter.Label(self.window, text="week_day").place(x=20, y=90)
-        tkinter.Entry(self.window).place(x=80, y=90)
-        ttk.Combobox(self.window, values=["shanbeh","yek shanbeh" , "do shanbeh" , "se shanbeh","chahar shanbeh","panj shanbeh"]).place(x=80, y=90)
 
-        tkinter.Label(self.window, text="start_date").place(x=20, y=130)
-        tkinter.Entry(self.window).place(x=80, y=130)
-
-        tkinter.Label(self.window, text="start_time").place(x=20, y=170)
-        tkinter.Entry(self.window).place(x=80, y=170)
-
-        tkinter.Label(self.window, text="end_time").place(x=20, y=210)
-        tkinter.Entry(self.window).place(x=80, y=210)
-
-        self.table = Table(self.window, ["Id", "title", "week_day", "start_date"], [60, 100, 100, 60], x=250, y=20)
+        self.table = Table(self.window, ["Id", "title", "week_day", "start_date","end_time"], [60, 100, 100, 60], 250, 20,self.table_click)
         self.table.refresh_table(LessonController.find_all()[1])
 
-        save_btn = tkinter.Button(self.window,
-                             text="save",
-                             bg="green",
-                             fg="black",
-                             font=("B titr", 10),
-                             width=10, command=self.save_click)
-        save_btn.place(x=50,y=400)
 
-        edit_btn = tkinter.Button(self.window,
-                             text="edit",
-                             bg="yellow",
-                             fg="black",
-                             font=("B titr", 10),
-                             width=10, command=self.edit_click)
-        edit_btn.place(x=180, y=400)
+        Button(self.window, text="Save", width=10, command=self.save_click).place(x=100, y=180)
+        Button(self.window, text="Edit", width=10, command=self.edit_click).place(x=100, y=210)
+        Button(self.window, text="Remove", width=10, command=self.remove_click).place(x=100, y=240)
 
-        remove_btn = tkinter.Button(self.window,
-                             text="remove",
-                             bg="red",
-                             fg="black",
-                             font=("B titr", 10) ,
-                             width=10, command=self.remove_click)
-        remove_btn.place(x=300, y=400)
-
-
-
+        self.reset_form()
 
         self.window.mainloop()
 
-
 ui = LessonView()
+
