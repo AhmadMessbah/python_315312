eb8837d85ceaeafb2ab3263ff8e1f6d437702d7d
NimaTheCreator <sanatinima5@gmail.com>
2024-10-06 12:46:03 +0330

    (commited by NimaTheCreator)

--- model/entity/product.py
+++ model/entity/product.py
@@ -1,86 +1,72 @@
-# python cant find it(error > no module named product validator)
-#from pefile import PrologEpilogOpSetFP
+import re
 
-# from model.entity.product_validation import *
-from model.tools.product_validation import ProductValidation
+from sqlalchemy import Column, Integer, String, Boolean
 
-
-
-class Product:
-    def __init__(self, id, name, brand, model,barcode,buy_price,sell_price):
-        self.id = id
-        self.name = name
-        self.brand = brand
-        self.model = model
-        self.barcode = barcode
-        self.buy_price = buy_price
-        self.sell_price = sell_price
-
-    def __repr__(self):
-        return f"{self.__dict__}"
-
-    def to_tuple(self):
-        return tuple(self.__dict__.values())
-
-    @property
-    def id(self):
-        return self.id
-
-    @id.setter
-    def id(self, id):
-        self.id = ProductValidation.id_validator(id, "Invalid id")
-
-    @property
-    def name(self):
-        return self.name
-
-    @name.setter
-    def name(self, name):
-        self.name = ProductValidation.name_validator(name, "Invalid Name")
-
-    @property
-    def brand(self):
-        return self.brand
-
-    @brand.setter
-    def brand(self, brand):
-        self.brand = ProductValidation.brand_validator(brand, "Invalid brand")
-
-    @property
-    def model(self):
-        return self.model
-
-    @model.setter
-    def model(self, model):
-        self.brand = ProductValidation.model_validator(model, "Invalid model")
-
-    @property
-    def barcode(self):
-        return self.barcode
-
-    @barcode.setter
-    def barcode(self, barcode):
-        self.barcode = ProductValidation.barcode_validator(barcode, "Invalid barcode")
-
-    @property
-    def buy_price(self):
-        return self.buy_price
-
-    @buy_price.setter
-    def buy_price(self, buy_price):
-        self.buy_price = ProductValidation.buy_sell_validator(buy_price, "Invalid buy price")
-
-    @property
-    def sell_price(self):
-        return self.sell_price
-
-    @sell_price.setter
-    def sell_price(self, sell_price):
-        self.sell_price = ProductValidation.buy_sell_validator(sell_price, "Invalid sell price")
+from model.entity.book_validation import *
+from model.entity.base import Base
+from model.entity.product_validation import no_numbers, string_validator
 
 
-# test
+class Product(Base):
+    __tablename__ = 'product'
+
+    _id = Column(Integer, primary_key=True, autoincrement=True)
+    _name = Column(String, nullable=False)
+    _brand = Column(String, nullable=False)
+    _model = Column(String, nullable=False)
+    _barcode = Column(String, nullable=False)
+    _buy_price = Column(Integer, nullable=False)
+    _sell_price = Column(Integer, nullable=False)
 
 
+    def __init__(self,id, name, brand, model, barcode, buy_price, sell_price):
+        self._id = id
+        self._name = name
+        self._brand = brand
+        self._model = model
+        self._barcode = barcode
+        self._buy_price = buy_price
+        self._sell_price = sell_price
 
+    def get_name(self):
+        return self._name
 
+    def set_name(self, name):
+        self._name = no_numbers(name)
+
+    def get_brand(self):
+        return self._brand
+
+    def set_brand(self, brand):
+        self._brand = no_numbers(brand)
+
+    def get_model(self):
+        return self._model
+
+    def set_model(self, model):
+        self._model = string_validator(model)
+
+    def get_barcode(self):
+        return self._barcode
+
+    def set_barcode(self, barcode):
+        self._barcode = string_validator(barcode)
+
+    def get_buy_price(self):
+        return self._buy_price
+
+    def set_buy_price(self, buy_price):
+        self._buy_price = positive_int(buy_price)
+
+    def get_sell_price(self):
+        return self._sell_price
+
+    def set_sell_price(self, sell_price):
+        self._sell_price = positive_int(sell_price)
+
+    name = property(get_name, set_name)
+    brand = property(get_brand, set_brand)
+    model = property(get_model, set_model)
+    barcode = property(get_barcode, set_barcode)
+    buy_price = property(get_buy_price, set_buy_price)
+    sell_price = property(get_sell_price, set_sell_price)
