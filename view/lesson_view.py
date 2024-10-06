cf24e8451c4c5aae807971d23f75904f0d8aeb29
Z-naderi1 <rasta.naderi.2022@gmail.com>
2024-10-07 00:42:03 +0330

    chenge some error a bout view and test from project change another view error

--- view/lesson_view.py
+++ view/lesson_view.py
@@ -2,7 +2,6 @@ import tkinter
 from tkinter import *
 import tkinter.ttk as ttk
 import tkinter.messagebox as msg
-
 from controller.lesson_controller import LessonController
 from view.component import LabelWithEntry, Table
 
@@ -27,7 +26,7 @@ class LessonView:
 
 
      def save_click(self):
-        status, message = LessonController.save(self.title.get(), self.week_day.get(), self.start_date.get())
+        status, message = LessonController.save(self.title.get(), self.week_day.get(), self.start_date.get(), self.start_time.get(),self.end_time.get())
         if status:
             msg.showinfo("Save",message)
             self.reset_form()
@@ -37,7 +36,7 @@ class LessonView:
 
 
     def edit_click(self):
-        status, message =LessonController.edit(self.id.get(),self.title.get(), self.week_day.get(), self.start_date.get(), self.age.get())
+        status, message =LessonController.edit(self.id.get(),self.title.get(), self.week_day.get(), self.start_date.get(), self.start_time.get(), self.end_time.get())
         if status:
             msg.showinfo("Edit", message)
             self.reset_form()
@@ -47,7 +46,7 @@ class LessonView:
 
 
     def remove_click(self):
-        if msg.askyesno("Remove Employee","are you sure?"):
+        if msg.askyesno("Remove Lesson","Are you sure you want to remove this lesson?"):
             status, message =LessonController.remove(self.id.get())
             if status:
                 msg.showinfo("Remove", message)
@@ -57,20 +56,20 @@ class LessonView:
 
 
     def __init__(self):
-        self.window = Tk()
-        self.window.geometry('500x500')
-        self.window.title("Lesson")
+        self.window =Tk()
+        self.window.geometry('600x400')
+        self.window.title("Lesson Management")
 
 
-        self.id = LabelWithEntry(self.window, "Id", 20, 20, data_type="int", state="readonly")
-        self.title = LabelWithEntry(self.window, "title", 20, 60)
-        self.week_day = LabelWithEntry(self.window, "week day", 20, 60)
-        self.start_time= LabelWithEntry(self.window, "start time", 20, 140, data_type="str")
-        self.start_date= LabelWithEntry(self.window,"Start Date", 20, 180, data_type="int")
-        self.end_time= LabelWithEntry(self.window, "End Date", 20, 200, data_type="str")
+        self.id = LabelWithEntry(self.window, "ID", 20, 20, data_type="int", state="readonly")
+        self.title = LabelWithEntry(self.window, "Title", 20, 60)
+        self.week_day = LabelWithEntry(self.window, "Week day", 20, 100)
+        self.start_time= LabelWithEntry(self.window, "Start time", 20, 140)
+        self.start_date= LabelWithEntry(self.window,"Start Date", 20, 180)
+        self.end_time= LabelWithEntry(self.window, "End Date", 20, 220)
 
 
-        self.table = Table(self.window, ["Id", "title", "week_day", "start_date","end_time"], [60, 100, 100, 60], 250, 20,self.table_click)
+        self.table = Table(self.window, ["ID", "Title", "Week_day", "Start_date","End_time"], [60, 100, 100, 80, 80], 250, 20,self.table_click)
         self.table.refresh_table(LessonController.find_all()[1])
 
 
@@ -79,7 +78,6 @@ class LessonView:
         Button(self.window, text="Remove", width=10, command=self.remove_click).place(x=100, y=240)
 
         self.reset_form()
-
         self.window.mainloop()
 
 ui = LessonView()
