df0c84f0bd8bfc30d94f4abb9c172949d1f8673f
AidaShams <aidashmas77@gmail.com>
2024-10-14 19:58:11 +0330

    payment test 2.0

--- test/payment_test.py
+++ test/payment_test.py
@@ -1,8 +1,9 @@
+from controller.payment_controller import PaymentController
 from model.entity.payment import Payment
 
+print(PaymentController.save(1, 12345567,  "Reza Rezaee"))
+#payment = Payment(1, 12345, 500, "Reza Rezaee")
 
-payment = Payment(1, 12345, 500, "Reza Rezaee")
+#print(payment)
 
-print(payment)
-
-print(payment.to_tuple())
+#print(payment.to_tuple())
